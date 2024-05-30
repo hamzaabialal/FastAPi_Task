1690d110ded70a565b34c48253b30c643b2a5e0d
hamzaabialal <hamzaabialal@gmail.com>
2024-05-30 16:49:08 +0500

    Adding Docker

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL =  "postgresql://myuser:mypass@localhost/myuser1"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
